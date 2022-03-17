59178ebd23e3beceb1fcce8bebf7a6f42d411726
Yinuo Meng <mengyn1999@gmail.com>
2022-03-17 15:17:27 +0000

    Customise blog django admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from blog.models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'author',
+                    'slug', 'status', 'originally_written_on')
+    list_filter = ('status', 'category')
+    search_fields = ['title', 'content', 'author']
+    prepopulated_fields = {'slug': ("title",)}
+    date_hierarchy = 'originally_written_on'
+
+
+admin.site.register(Post, PostAdmin)
