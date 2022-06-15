13fff9752b331d105a27c4e925471d4debb783f9
Yinuo Meng <mengyn1999@gmail.com>
2022-06-15 21:51:09 +0100

    - Add django-typomatic and apply it to FragmentSerializer

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from blog.models import Fragment
+from django_typomatic import ts_interface, generate_ts
 
 
+@ts_interface()
 class FragmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fragment
         exclude = ['title', 'created_by', 'updated_on', 'created_on']
+
+
+generate_ts('../frontend/src/constants/serializers.ts')
